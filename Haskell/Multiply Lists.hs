53f5a19691ccc816627b01f23662f676a92ac0bb
luisgepeto <luisgepeto@gmail.com>
2016-12-16 09:08:36 -0600

    added solvedmultiply lists problem

---
+++ Haskell/Multiply Lists.hs
@@ -0,0 +1,16 @@
+import System.Environment (getArgs)
+import Data.List
+
+main = do
+    [inpFile] <- getArgs    
+    input <- readFile inpFile        
+    mapM_ multiplyLines $ lines input
+
+multiplyLines :: String -> IO ()
+multiplyLines line = do
+    let Just pipeIndex = elemIndex '|' line
+        (firstListRaw, '|':secondListRaw) = splitAt pipeIndex line       
+        firstListAsString = words firstListRaw
+        secondListAsString = words secondListRaw
+        multipliedList = zipWith (\x y -> show ((read x) * (read y))) firstListAsString secondListAsString
+    putStrLn  $ unwords multipliedList
